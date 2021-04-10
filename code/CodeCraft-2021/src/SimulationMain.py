97ab6113bdea9b59ee2881f70d1269cf4810014c
enderman19980125 <enderman19980125@outlook.com>
2021-04-10 17:35:46 +0800

    Method: PurchaseServer. Formula:     0% - 20% days: cost_everyday.     20% - 80% days: cost_purchase + cost_day * rest_day.     80% - 100% days: cost_purchase. Cost: 20,6823,5959.

--- code/CodeCraft-2021/src/SimulationMain.py
+++ code/CodeCraft-2021/src/SimulationMain.py
@@ -1,11 +1,16 @@
 from _ServerConfig import ServerConfig
 from _Environment import Environment
 from _Operation import *
+from typing import Tuple, List
 
+
+# --------------------------------    initialize    --------------------------------
 
 def init(**kwargs) -> None:
     pass
 
+
+# --------------------------------    migrate    --------------------------------
 
 def migrate_vm(e: Environment, vm: Union[SingleVM, DoubleVM], current_server: Server) -> int:
     if isinstance(vm, SingleVM):
@@ -51,12 +56,87 @@ def migrate_current_day(e: Environment, **kwargs) -> None:
     #         break
 
 
+# --------------------------------    position    --------------------------------
+
+def server_cost_s(vm: SingleVM, server: Server, node: str) -> float:
+    p = 1.0
+    if node == 'A':
+        return p * server.get_rest_cpu_of_node_a() + server.get_rest_memory_of_node_a() + \
+               - p * vm.get_cpu_of_one_node() - vm.get_memory_of_one_node()
+    else:
+        return p * server.get_rest_cpu_of_node_b() + server.get_rest_cpu_of_node_b() + \
+               - p * vm.get_cpu_of_one_node() - vm.get_memory_of_one_node()
+
+
+def server_cost_d(vm: DoubleVM, server: Server) -> float:
+    p = 1.0
+    return p * max(server.get_rest_cpu_of_node_a(), server.get_rest_cpu_of_node_b()) + \
+           max(server.get_rest_memory_of_node_a(), server.get_rest_memory_of_node_b()) + \
+           - p * vm.get_cpu_of_one_node() - vm.get_memory_of_one_node()
+
+
+def select_running_server_for_single_vm(e: Environment, vm: SingleVM) -> Tuple[Optional[Server], Optional[str]]:
+    target_server = None
+    target_node = None
+
+    for server in e.get_deployed_server_dict().values():
+        if server.has_capacity_for_single_vm(vm=vm, node='A'):
+            if target_server is None or server_cost_s(vm, server, 'A') < server_cost_s(vm, target_server, 'A'):
+                target_server, target_node = server, 'A'
+        if server.has_capacity_for_single_vm(vm=vm, node='B'):
+            if target_server is None or server_cost_s(vm, server, 'B') < server_cost_s(vm, target_server, 'B'):
+                target_server, target_node = server, 'B'
+
+    return target_server, target_node
+
+
+def select_idle_server_for_single_vm(e: Environment, vm: SingleVM) -> Tuple[Optional[Server], Optional[str]]:
+    target_server = None
+    target_node = None
+
+    for server in e.get_non_deployed_server_dict().values():
+        if server.has_capacity_for_single_vm(vm=vm, node='A'):
+            if target_server is None or server_cost_s(vm, server, 'A') < server_cost_s(vm, target_server, 'A'):
+                target_server, target_node = server, 'A'
+
+    return target_server, target_node
+
+
+def select_running_server_for_double_vm(e: Environment, vm: DoubleVM) -> Optional[Server]:
+    target_server = None
+
+    for server in e.get_deployed_server_dict().values():
+        if server.has_capacity_for_double_vm(vm=vm):
+            if target_server is None or server_cost_d(vm, server) < server_cost_d(vm, target_server):
+                target_server = server
+
+    return target_server
+
+
+def select_idle_server_for_double_vm(e: Environment, vm: DoubleVM) -> Optional[Server]:
+    target_server = None
+
+    for server in e.get_non_deployed_server_dict().values():
+        if server.has_capacity_for_double_vm(vm=vm):
+            if target_server is None or server_cost_d(vm, server) < server_cost_d(vm, target_server):
+                target_server = server
+
+    return target_server
+
+
+# --------------------------------    purchase    --------------------------------
+
 def server_config_cost(e: Environment, server_config: ServerConfig) -> float:
-    return server_config.get_cost_purchase() + \
-           1.0 * server_config.get_cost_everyday() * (e.get_total_days() - e.get_current_day())
+    if e.get_current_day() / e.get_total_days() < 0.2:
+        return server_config.get_cost_everyday()
+    elif e.get_current_day() / e.get_total_days() < 0.8:
+        return server_config.get_cost_purchase() + \
+               server_config.get_cost_everyday() * (e.get_total_days() - e.get_current_day())
+    else:
+        return server_config.get_cost_purchase()
 
 
-def purchase_server(e: Environment, vm: Union[SingleVM, DoubleVM]) -> Server:
+def purchase_the_best_server(e: Environment, vm: Union[SingleVM, DoubleVM]) -> Server:
     target_server_config = None
 
     for server_config in e.get_server_config_dict().values():
@@ -73,53 +153,57 @@ def purchase_server(e: Environment, vm: Union[SingleVM, DoubleVM]) -> Server:
     return server
 
 
-def simulate_current_day(e: Environment, **kwargs) -> None:
-    for op in e.get_current_day_info().get_request_operation_list():
+# --------------------------------    simulate    --------------------------------
+
+def simulate_current_stage(e: Environment,
+                           deploy_operations_list: List[Union[DeploySingleVMOperation, DeployDoubleVMOperation]],
+                           **kwargs) -> None:
+    deploy_operations_list.sort(
+        key=lambda op: op.get_vm().get_cpu_of_one_node() + op.get_vm().get_memory_of_one_node(),
+        reverse=True
+    )
+
+    for op in deploy_operations_list:
         vm = op.get_vm()
 
         if isinstance(op, DeploySingleVMOperation):
-            for server in e.get_deployed_server_dict().values():
-                if server.has_capacity_for_single_vm(vm=vm, node='A'):
-                    e.op_deploy_single_vm(op=op, server=server, node='A')
-                    break
-                if server.has_capacity_for_single_vm(vm=vm, node='B'):
-                    e.op_deploy_single_vm(op=op, server=server, node='B')
-                    break
+            server, node = select_running_server_for_single_vm(e=e, vm=vm)
+            if server:
+                e.op_deploy_single_vm(op=op, server=server, node=node)
+                continue
 
-            if op.get_server() is None:
-                for server in e.get_non_deployed_server_dict().values():
-                    if server.has_capacity_for_single_vm(vm=vm, node='A'):
-                        e.op_deploy_single_vm(op=op, server=server, node='A')
-                        break
-                    if server.has_capacity_for_single_vm(vm=vm, node='B'):
-                        e.op_deploy_single_vm(op=op, server=server, node='B')
-                        break
+            server, node = select_idle_server_for_single_vm(e=e, vm=vm)
+            if server:
+                e.op_deploy_single_vm(op=op, server=server, node=node)
+                continue
 
-            if op.get_server() is None:
-                server = purchase_server(e=e, vm=vm)
-                e.op_deploy_single_vm(op=op, server=server, node='A')
+            server = purchase_the_best_server(e=e, vm=vm)
+            e.op_deploy_single_vm(op=op, server=server, node='A')
 
         elif isinstance(op, DeployDoubleVMOperation):
-            for server in e.get_deployed_server_dict().values():
-                if server.has_capacity_for_double_vm(vm=vm):
-                    e.op_deploy_double_vm(op=op, server=server)
-                    break
-
-            if op.get_server() is None:
-                for server in e.get_non_deployed_server_dict().values():
-                    if server.has_capacity_for_double_vm(vm=vm):
-                        e.op_deploy_double_vm(op=op, server=server)
-                        break
-                    if server.has_capacity_for_double_vm(vm=vm):
-                        e.op_deploy_double_vm(op=op, server=server)
-                        break
-
-            if op.get_server() is None:
-                server = purchase_server(e=e, vm=vm)
+            server = select_running_server_for_double_vm(e=e, vm=vm)
+            if server:
                 e.op_deploy_double_vm(op=op, server=server)
+                continue
 
-        elif isinstance(op, RemoveSingleVMOperation):
-            e.op_remove_vm(op=op)
+            server = select_idle_server_for_double_vm(e=e, vm=vm)
+            if server:
+                e.op_deploy_double_vm(op=op, server=server)
+                continue
 
-        elif isinstance(op, RemoveDoubleVMOperation):
+            server = purchase_the_best_server(e=e, vm=vm)
+            e.op_deploy_double_vm(op=op, server=server)
+
+
+def simulate_current_day(e: Environment, **kwargs) -> None:
+    deploy_operations_list = []
+
+    for op in e.get_current_day_info().get_request_operation_list():
+        if isinstance(op, RemoveSingleVMOperation) or isinstance(op, RemoveDoubleVMOperation):
+            simulate_current_stage(e=e, deploy_operations_list=deploy_operations_list)
+            deploy_operations_list.clear()
             e.op_remove_vm(op=op)
+        else:
+            deploy_operations_list.append(op)
+
+    simulate_current_stage(e=e, deploy_operations_list=deploy_operations_list)
